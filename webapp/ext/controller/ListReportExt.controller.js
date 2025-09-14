b4cc3143bb2d62eced977554b8d7f76573568220
FABIAN DIZ <fdiz@artech-consulting.com>
2025-09-13 21:17:21 -0300

    dale

--- webapp/ext/controller/ListReportExt.controller.js
+++ webapp/ext/controller/ListReportExt.controller.js
@@ -1,9 +1,31 @@
-sap.ui.define([], () => {
+sap.ui.define([
+    "sap/ui/core/Fragment"
+], (Fragment) => {
     "use strict"
     return {
         onInit () {
             alert("Controller ListReportExt initialized")
+        },
+        onOpenButton () {
+            alert("Button pressed");
+            if (this._oFragment) return this._oFragment.open();
+            Fragment.load({
+                id: this.getView().getId(),
+                name: "clase6.ext.fragments.Example",
+                // name: "ext/fragments/Example",
+                controller: this,
+            }).then( function (oUploadFragment) { 
+                this._oFragment = oUploadFragment;
+                this.getView().addDependent(this._oFragment);
+                this._oFragment.open();
+            }.bind(this)
+        );
+        },
+        onExit () {
+            this._oFragment.close();
         }
+
+
     }
 }
 )
